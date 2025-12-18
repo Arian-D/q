823e51dc8929381fbef34b49f07c074258f5fa53
Arian <arianxdehghani@gmail.com>
2025-12-18 13:59:34 -0800

    Add tools

--- chat.tsx
+++ chat.tsx
@@ -1,32 +1,64 @@
 import { Alert, TextInput } from "@inkjs/ui";
+import { DuckDuckGoSearch } from "@langchain/community/tools/duckduckgo_search";
+import { WikipediaQueryRun } from "@langchain/community/tools/wikipedia_query_run";
+import { ChatOllama } from "@langchain/ollama";
 import { Box, render, Text } from "ink";
+import { createAgent } from "langchain";
 import React, { type ReactNode, useEffect, useState } from "react";
-import { Ollama } from "@langchain/ollama";
 
-interface OllamaTextProps {
+function createOllamaAgent() {
+  const model = new ChatOllama({
+    baseUrl: "http://localhost:11434",
+    // TODO: Dynamically pick based on prompt and resources
+    model: "qwen3:1.7b",
+    maxRetries: 1,
+  });
+
+  // TODO: Use a proper type
+  const tools: any[] = [
+    // new DuckDuckGoSearch({ maxResults: 3 }),
+    new WikipediaQueryRun({
+      topKResults: 3,
+      maxDocContentLength: 4000,
+    }),
+  ];
+  // TODO: Add MCPs
+
+  const agent = createAgent({
+    model: model,
+    tools,
+  });
+
+  return agent;
+}
+
+const agent = createOllamaAgent();
+
+interface PromptProps {
   prompt: string;
 }
 
-function OllamaText({ prompt }: OllamaTextProps) {
+function OllamaText({ prompt }: PromptProps) {
   const [response, setResponse] = useState("");
   const [error, setError] = useState(null);
   useEffect(() => {
-    const ollama = new Ollama({
-      baseUrl: "http://localhost:11434",
-      // TODO: Pick the model based on system resources
-      model: "goekdenizguelmez/JOSIEFIED-Qwen3:1.7b",
-    });
-    ollama.stream(
-      prompt,
-    ).then(async (result) => {
-      for await (const chunk of result) {
-        setResponse((prev: string) => prev + chunk);
-      }
-    }).catch((err) => {
-      setError(err);
-    });
+    agent.stream({
+      messages: [{
+        role: "user",
+        content: prompt,
+      }],
+    }, { streamMode: "messages" }).then(
+      async (result) => {
+        for await (const [token, metadata] of result) {
+          if (metadata.langgraph_node === "model_request") {
+            setResponse((prev: string) => prev + token.content);
+          }
+        }
+      },
+    );
   }, [prompt]);
   if (error) {
+    // TODO: Figure out how to get around the child issue on deno
     const children: ReactNode = String(error);
     // deno-lint-ignore jsx-no-children-prop
     return <Alert variant="error" children={children} />;
@@ -37,30 +69,34 @@ function OllamaText({ prompt }: OllamaTextProps) {
 function Main() {
   const [value, setValue] = useState("");
   const [submitted, setSubmitted] = useState(false);
-  return (
-    <Box flexDirection="column">
-      <Box
-        padding={0}
-        gap={1}
-        borderStyle="round"
-        borderColor="green"
-      >
-        <TextInput
-          isDisabled={submitted}
-          placeholder="Prompt..."
-          onChange={setValue}
-          onSubmit={() => setSubmitted(true)}
-        />
+  if (value == "/") {
+    // TODO: Add model picker
+  } else {
+    return (
+      <Box flexDirection="column">
+        <Box
+          padding={0}
+          gap={1}
+          borderStyle="round"
+          borderColor="green"
+        >
+          <TextInput
+            isDisabled={submitted}
+            placeholder="Prompt..."
+            onChange={setValue}
+            onSubmit={() => setSubmitted(true)}
+          />
+        </Box>
+        <Box
+          display={submitted ? "flex" : "none"}
+          borderStyle="round"
+          borderColor="blue"
+        >
+          {submitted && <OllamaText prompt={value} />}
+        </Box>
       </Box>
-      <Box
-        display={submitted ? "flex" : "none"}
-        borderStyle="round"
-        borderColor="blue"
-      >
-        {submitted && <OllamaText prompt={value} />}
-      </Box>
-    </Box>
-  );
+    );
+  }
 }
 
 render(<Main />);
